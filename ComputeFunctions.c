677a037bb862eb5b91bdee4bb2785ca2ce00d337
rufus20145 <35730338+rufus20145@users.noreply.github.com>
2019-03-10 17:44:44 +0300

    Add files via upload

---
+++ ComputeFunctions.c
@@ -0,0 +1,25 @@
+double computeDivision(double firstNumber, double secondNumber)
+{
+    if (0 == secondNumber)
+    {
+        printf("It`s impossible to divide by 0");
+        return ;
+    }
+    return firstNumber/secondNumber;
+}
+int computeDivisionWithRemainder(double firstNumber, double secondNumber)
+//int or double?
+{
+    if (0 == secondNumber)
+    {
+        printf("It`s impossible to divide by 0"); 
+    }
+}
+int computeQuotientOfDivision(double firstNumber, double secondNumber)
+//int or double?
+{
+    if ( 0 == secondNumber)
+    {
+        printf("It`s impossible to divide by 0");
+    }
+}
