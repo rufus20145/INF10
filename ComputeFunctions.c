792cc63d3a2527efeb53d120581fdafa926e0d01
rufus20145 <35730338+rufus20145@users.noreply.github.com>
2019-03-11 13:06:32 +0300

    Add files via upload
    Вынес прогрессию в отдельный файл из-за нехватки строк

--- ComputeFunctions.c
+++ ComputeFunctions.c
@@ -62,13 +62,3 @@ int computeQuotientOfDivision(double firstNumber, double secondNumber)
     return firstNum/secondNum;
 
 }
-/*
-@param firstNumber - a0, secondNumber - множитель прогрессии
-@return возвращает члены, сумму и произведение последовательности
-@example 2 G 2, N = 3(вводится дополнительно) => члены последовательности: 2,4,8,16;
-сумма последовательности: 30; произведение последовательности: 1024
-*/
-double computeProgression(double firstNumber, double secondNumber)
-{
-    
-}
---
+++ Progression.c
@@ -0,0 +1,10 @@
+/*
+@param firstNumber - a0, secondNumber - множитель прогрессии
+@return возвращает члены, сумму и произведение последовательности
+@example 2 G 2, N = 3(вводится дополнительно) => члены последовательности: 2,4,8,16;
+сумма последовательности: 30; произведение последовательности: 1024
+*/
+double computeProgression(double firstNumber, double secondNumber)
+{
+    
+}
