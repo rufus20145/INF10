dc08247959c68680b42ae258775471186c0fdf58
rufus20145 <35730338+rufus20145@users.noreply.github.com>
2019-03-11 07:30:39 +0300

    Add files via upload

--- ComputeFunctions.c
+++ ComputeFunctions.c
@@ -1,3 +1,20 @@
+/*
+Дополнительный файл с сложными функциями
+Проектное задание №0
+Калькулятор со следующими возможностями:
+Сложение, вычитание, умножение, деление, деление нацело с взятием остатка,
+деления нацело со взятием результата-частного, геометрическая последовательность
+
+@author Ivan.rufus20145
+@link https://github.com/rufus20145/My_calc
+@link http://moria.1586.su/moodle/mod/page/view.php?id=1705
+*/
+
+/*
+@param firstNumber - делимое, secondNum - делитель
+@return возвращает частное двух чисел, если возможно
+@example деление чисел: 6 / 4 = 1.5
+*/
 double computeDivision(double firstNumber, double secondNumber)
 {
     if (0 == secondNumber)
@@ -7,6 +24,11 @@ double computeDivision(double firstNumber, double secondNumber)
     }
     return firstNumber/secondNumber;
 }
+/*
+@param firstNumber - делимое, secondNum - делитель
+@return возвращает остаток от деления, если возможно
+@example 6 % 4 = 2
+*/
 int computeDivisionWithRemainder(double firstNumber, double secondNumber)
 //int or double?
 {
@@ -15,6 +37,11 @@ int computeDivisionWithRemainder(double firstNumber, double secondNumber)
         printf("It`s impossible to divide by 0"); 
     }
 }
+/*
+@param firstNumber - делимое, secondNum - делитель
+@return возвращает неполное частное от деления, если возможно
+@example 6 D 4 = 1
+*/
 int computeQuotientOfDivision(double firstNumber, double secondNumber)
 //int or double?
 {
@@ -22,4 +49,14 @@ int computeQuotientOfDivision(double firstNumber, double secondNumber)
     {
         printf("It`s impossible to divide by 0");
     }
+}
+/*
+@param firstNumber - a0, secondNumber - множитель прогрессии
+@return возвращает члены, сумму и произведение последовательности
+@example 2 G 2, N = 3(вводится дополнительно) => члены последовательности: 2,4,8,16;
+сумма последовательности: 30; произведение последовательности: 1024
+*/
+double computeMemberOfSequence(double firstNumber, double secondNumber)
+{
+    
 }
--- calc.c
+++ calc.c
@@ -3,7 +3,35 @@
 Калькулятор со следующими возможностями:
 Сложение, вычитание, умножение, деление, деление нацело с взятием остатка,
 деления нацело со взятием результата-частного, геометрическая последовательность
-@link 
+
+@author Ivan.rufus20145
+@link https://github.com/rufus20145/My_calc
+@link http://moria.1586.su/moodle/mod/page/view.php?id=1705
 */
 #include <stdio.h>
 #include <ComputeFunctions.c>
+int main()
+{
+    double firstNumber, secondNumber;
+    char operation;
+    while(true) {
+            scanf("%lg%c%lg", &firstNumber, &operation, &secondNumber);
+            switch (operation)
+            {
+                case '+':
+                    printf("%lg + %lg = %ln\n", firstNumber, secondNumber, firstNumber+secondNumber);
+                    break;
+                case '-':
+                    printf("%lg - %lg = %lg\n", firstNumber, secondNumber, firstNumber-secondNumber);
+                    break;
+                case '*':
+                    printf("%lg * %lg = %lg\n", firstNumber, secondNumber, firstNumber*secondNumber);
+                    break;
+                case '/':
+                    printf("%lg / %lg = %lg\n", firstNumber, secondNumber, computeDivision(firstNumber, secondNumber) );
+                    break;
+                default:
+                    break;
+            }
+    }
+}
