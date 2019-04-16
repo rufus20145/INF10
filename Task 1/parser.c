aa29ceaf14154a2fd06924b758063ef582a008b7
rufus20145 <ivan20027749@gmail.cpm>
2019-04-16 19:39:24 +0300

    Реорганизация 2

---
+++ Task 1/parser.c
@@ -0,0 +1,26 @@
+/**
+ * Файл с функциями парсинга строк
+ * Калькулятор с функцией посчета корня линейной функции.
+ * @link https://github.com/rufus20145/My-calc-2
+ * @link http://moria.1586.su/moodle/mod/page/view.php?id=1709
+ * @author Ivan.rufus20145
+ */
+
+#define MAX_V 10
+
+/**
+ * @param char* ctring[] - указатель на строку для парсинга
+ * @return void
+ */
+int parseFunction(char strin[], char* funcX, char* funcY, int* k, int* b) {
+}
+
+/**
+ * @param char* string[] - указатель на строку для парсинга
+ * @return void
+ */
+
+int parseSet(char string[], int values[MAX_V], char setX[MAX_V], int setNum) {
+    strcpy(setX[setNum], string[4]);
+    printf(setX[setNum]);
+}
